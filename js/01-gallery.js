d5bc7e284d07e988815dea0e7e37c0a6bc593900
jmatela26 <jmatela26@gmail.com>
2024-01-09 23:57:01 +0800

    Added fixes for the simple light box

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
+const galleryMarkup = galleryItems.map((image) => 
+`<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+const onContainerClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery__image")) {
+    const source = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${source}" width="800" height="600">`);
+
+    instance.show();
+  }
+};
+
+galleryList.addEventListener("click", onContainerClick);
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map((image) => 
+`<li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+     <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+</li>`
+).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Initialize SimpleLightbox after gallery items are added
+const lightbox = new SimpleLightbox('.gallery a');
+
